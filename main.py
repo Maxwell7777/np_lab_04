665804b2f33418d396374b17746a51629a61757a
Max Kłosiński <ezreal7294@gmail.com>
2024-01-03 11:40:27 +0100

    commit dla test10

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def multiply_numbers(a,b):
 
 def divide_numbers(a, b):
     if b== 0:
-        raise ValueError("DDzielenie przez zero jest niedozwolone.")
+        raise ValueError("DDDzielenie przez zero jest niedozwolone.")
     return a /b
 
 if __name__ == "__main__":
