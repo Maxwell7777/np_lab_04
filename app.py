2bd4297b10f42806f01941e959f6d89293597f53
Maxwell7777 <65621087+Maxwell7777@users.noreply.github.com>
2024-01-04 13:40:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 def add_numbers(a, b):
-<<<<<<< HEAD
     return a+b
 def subtract_numbers(a , b):
     return a - b
@@ -17,22 +16,3 @@ if __name__ == "__main__":
     print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
     print(f"Mnożenie: {multiply_numbers(num1, num2)}")
     print(f"Dzielenie: {divide_numbers(num1, num2)}")
-
-=======
-    return a + b
-def subtract_numbers(a, b):
-    return a - b
-def multiply_numbers(a, b):
-    return a * b
-def divide_numbers(a, b):
-    if b == 0:
-        raise ValueError("Dzielenie przez zero jest niedozwolone.")
-    return a / b
-if __name__ == "__main__":
- num1 = 10
- num2 = 5
- print(f"Dodawanie: {add_numbers(num1, num2)}")
- print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
- print(f"Mnożenie: {multiply_numbers(num1, num2)}")
- print(f"Dzielenie: {divide_numbers(num1, num2)}")
->>>>>>> e533da87127c19d41a472b19c04e96ceefcd44c9
